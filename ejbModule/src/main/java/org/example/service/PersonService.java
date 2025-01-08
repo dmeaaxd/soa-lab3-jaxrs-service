26ad1dc3339e12913f555859a2f4e154d5d3751a
Daniil Kalinin <dan_kalinin_03@mail.ru>
2025-01-08 22:11:32 +0300

    delete request to spring service

--- ejbModule/src/main/java/org/example/service/PersonService.java
+++ ejbModule/src/main/java/org/example/service/PersonService.java
@@ -1,43 +1,34 @@
 package org.example.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.BadRequestException;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.Response;
-import org.example.dto.response.ErrorResponseDto;
+import jakarta.ws.rs.InternalServerErrorException;
+import jakarta.ws.rs.NotFoundException;
 import org.example.entity.Person;
+import org.example.exception.DatabaseException;
 import org.example.exception.SpringServiceException;
+import org.example.repository.PersonRepository;
 
 @ApplicationScoped
 public class PersonService {
 
-    private final Client client = ClientBuilder.newClient();
-    private final String url = "https://hahaha.drunkenhedgehog.ru/killer";
+    @Inject
+    private PersonRepository personRepository;
 
-    public Person findById(Integer id) throws BadRequestException, SpringServiceException {
+    public Person getPersonById(Integer id) throws BadRequestException, SpringServiceException {
         if (id == null) {
             throw new BadRequestException("Id can not be null");
         }
-        Response response;
 
         try {
-            WebTarget target = client.target(url + "/" + id);
-            response =  target.request().get();
-        } catch (Exception e) {
-            throw new SpringServiceException(e.getMessage());
-        }
-
-        if (response.getStatus() != 200) {
-            if (response.getStatus() == 404) {
-                return null;
+            Person person =  personRepository.findById(id);
+            if (person == null) {
+                throw new NotFoundException("Person not found");
             }
-
-            String message = response.readEntity(ErrorResponseDto.class).getMessage();
-            throw new SpringServiceException(message);
+            return person;
+        } catch (DatabaseException databaseException){
+            throw new InternalServerErrorException(databaseException.getMessage());
         }
-
-        return response.readEntity(Person.class);
     }
 }
